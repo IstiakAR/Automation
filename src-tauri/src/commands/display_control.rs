f5b3093ecb0021bc75bddf5cd8930f6d1b18a43c
IstiakAR <istiakrhyme840@gmail.com>
2025-12-08 11:35:32 +0600

    changed screenshot crate

--- src-tauri/src/commands/display_control.rs
+++ src-tauri/src/commands/display_control.rs
@@ -1,24 +1,30 @@
 pub fn take_screenshot(save_path: &str) -> std::io::Result<String> {
-    use screenshots::Screen;
-    use image::{ImageEncoder, RgbaImage};
-    use std::fs::File;
-    use std::io::BufWriter;
-    
-    let screens = Screen::all().map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e))?;
-    
-    let screen = screens.into_iter()
-        .max_by_key(|s| s.display_info.width * s.display_info.height)
-        .ok_or(std::io::Error::new(std::io::ErrorKind::NotFound, "No screen found"))?;
-    
-    let image = screen.capture().map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e))?;
-    
-    println!("Captured image: {}x{}", image.width(), image.height());
-    
-    let rgba_image = RgbaImage::from_raw(image.width(), image.height(), image.as_raw().to_vec())
-        .ok_or(std::io::Error::new(std::io::ErrorKind::Other, "Failed to create image"))?;
-    
-    rgba_image.save(save_path)
+    use xcap::Monitor;
+    use std::path::Path;
+
+    let monitors = Monitor::all().map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e))?;
+
+    let monitor = monitors
+        .into_iter()
+        .max_by_key(|m| {
+            let w = m.width().unwrap_or(0);
+            let h = m.height().unwrap_or(0);
+            w * h
+        })
+        .ok_or(std::io::Error::new(
+            std::io::ErrorKind::NotFound,
+            "No monitor found",
+        ))?;
+
+    let image = monitor
+        .capture_image()
         .map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e))?;
-    
+
+    println!("Captured image: {}x{}", image.width(), image.height());
+
+    image
+        .save(Path::new(save_path))
+        .map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e))?;
+
     Ok(save_path.to_string())
 }
